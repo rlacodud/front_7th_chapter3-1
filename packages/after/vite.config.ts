d335c2c25b5f71f702e8a3a0c4898aff276d6421
rlacodud <rlacodud0313@naver.com>
2025-11-26 10:34:31 +0900

    chore: Tailwind 및 Storybook 의존성 추가
    - Tailwind CSS 추가
    - Storybook 추가
    - postcss.config.js 제거

--- packages/after/vite.config.ts
+++ packages/after/vite.config.ts
@@ -1,19 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+
+const base: string =
+  process.env.NODE_ENV === "production" ? "/front_7th_chapter3-1/" : "";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base,
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
-    css: true,
-  },
-})
+});
