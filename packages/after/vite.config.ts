9c80e77d345190017f1bde47dca76a9cdbbb046a
rlacodud <rlacodud0313@naver.com>
2025-11-26 10:44:38 +0900

    chore: 테스트 환경 설정

--- packages/after/vite.config.ts
+++ packages/after/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
@@ -7,12 +8,22 @@ const base: string =
   process.env.NODE_ENV === "production" ? "/front_7th_chapter3-1/" : "";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base,
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default mergeConfig(
+  defineConfig({
+    base,
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-});
+  }),
+  defineTestConfig({
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/test/setup.ts",
+      css: true,
+    },
+  })
+);
